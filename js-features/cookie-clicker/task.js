9e3112fde9e352ef08ba0bbcf415d9c92f601990
Oxana Galyamova <mrs.oxas@gmail.com>
2022-01-17 00:45:42 +0500

    task 2.1

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,12 @@
+//'use strict';
+
+let click = document.getElementById("clicker__counter");
+const cookieImg = document.getElementById("cookie");
+
+function cookieClicker(){
+    click.textContent ++;
+    cookieImg.width === 200 ? cookieImg.width / 2 : cookieImg.width * 2;
+    
+}
+
+cookieImg.onclick = cookieClicker;
--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -3,7 +3,7 @@
 const countDownTimer = setInterval(function() {
     let countDownStart = document.getElementById('timer');
     countDownStart.textContent --;
-    if(countDownStart.textContent <= 0){
+    if(countDownStart.textContent < 0){
         alert("Вы победили в конкурсе!");
         clearInterval(countDownTimer);
     }  
